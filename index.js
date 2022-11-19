5154d863039b78e3b8d2d2615b66aa9990e411dd
Anderson Rodrigo <andersonrodrigoam@gmail.com>
2022-11-19 13:55:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get("/", (req, res, next) => {
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
+// const YourDatabaseURLHere = "";
+
 mongoose
   .connect(
    // `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.{{YourDatabaseURLHere}}`
